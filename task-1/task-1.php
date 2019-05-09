84924cf4345c50d51071bc7b0503173e0d9e3c4e
bodyarty <50432554+bodyarty@users.noreply.github.com>
2019-05-09 06:36:18 +0300

    Delete task-1.php

--- task-1/task-1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$febanachi_array = array('0','1');
-for ($i = 2; $i < 64; $i++) {
-   $febanachi_array[$i] = bcadd($febanachi_array[$i-1], $febanachi_array[$i-2]);
-}
-echo implode("<br/>", $febanachi_array);
-
-?>
